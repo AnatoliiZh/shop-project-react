c9a1f532af46128476d1efed973ae6d0dd94be48
Yaroslav <kznkv.test@gmail.com>
2022-12-19 20:15:17 +0200

    add React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const Title = () => {
+    return <h1>Hello React</h1>
+}
+
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Consectetur architecto ab ea cupiditate corrupti nulla aliquam
+                aliquid tempora dignissimos, impedit, aut aspernatur natus optio
+                nostrum nesciunt officiis ipsum? Quibusdam, dicta.
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Consectetur architecto ab ea cupiditate corrupti nulla aliquam
+                aliquid tempora dignissimos, impedit, aut aspernatur natus optio
+                nostrum nesciunt officiis ipsum? Quibusdam, dicta.
+            </p>
+        </React.Fragment>
+    )
+}
+
+const App = () => {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
