614b5f1fb60ad9bf1058c014a7b5f382f7f35854
AnatoliiZh <abcland2014@gmail.com>
2022-12-21 19:24:44 +0200

    clear

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Title } from './Title'
-
-type ContentProps = {
-    p1: string
-    p2: string
-}
-
-const Content = (props: ContentProps) => {
-    return (
-        <React.Fragment>
-            <p>{props.p1}</p>
-            <p>{props.p2}</p>
-        </React.Fragment>
-    )
-}
-
-const App = () => {
-    return (
-        <>
-            <Title title="React" />
-            <Title title="TS" />
-            <Title title={10} />
-            <Content
-                p1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facilis quaerat sapiente offic"
-                p2="oiho oiyo iyoiyo ioiy"
-            />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
