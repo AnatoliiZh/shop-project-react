f45e95b9d95d3f67bf01f8abf2468818528827f5
AnatoliiZh <abcland2014@gmail.com>
2022-12-21 20:41:23 +0200

    add logo

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,10 +4,11 @@ type Props = {}
 const Menu = (props: Props) => {
     return (
         <>
-            <Button color="inherit">Login</Button>
+            <Button color="inherit">Home</Button>
             <Button color="inherit">Products</Button>
+            <Button color="inherit">Shipping</Button>
             <Button color="inherit">Payment</Button>
-            <Button color="inherit">Login</Button>
+            <Button color="inherit">Cart</Button>
         </>
     )
 }
