66d95ba729b3e4d8377006758ce86164ef58f18a
AnatoliiZh <abcland2014@gmail.com>
2022-12-23 19:41:47 +0200

    add ListItem

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -4,7 +4,7 @@ import Home from 'Pages/Home/Home'
 type Props = {}
 const Main = (props: Props) => {
     return (
-        <Container maxWidth="lg">
+        <Container maxWidth="lg" sx={{ padding: '50px 0' }} component="main">
             <Home />
         </Container>
     )
