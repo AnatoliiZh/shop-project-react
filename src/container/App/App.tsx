1f941ec9c5907acd72f6bc59ebaebdfce43faffc
AnatoliiZh <abcland2014@gmail.com>
2023-01-02 20:37:11 +0200

    add products in cart with button but in cart NaN

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -14,13 +14,18 @@ const App = () => {
         2: 1,
     })
 
-    const addProductToCart = (count: number, price: number) => {}
+    const addProductToCart = (id: number, count: number) => {
+        setProductsInCart((prevState: ProductsInСart) => ({
+            ...prevState,
+            [id]: prevState[id] + count,
+        }))
+    }
 
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInСart={productsInСart} />
-            {/* <button onClick={() => addProductToCart(5, 500)}></button> */}
+            <button onClick={() => addProductToCart(1, 1)}>Add to cart</button>
             <Main addProductToCart={addProductToCart} />
         </StyledEngineProvider>
     )
