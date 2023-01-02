401fb206173db3a066438cac63b57e97b91c30e9
Yaroslav <kznkv.test@gmail.com>
2023-01-02 18:57:58 +0200

    add addProductToCart

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -17,11 +17,18 @@ const App = (props: Props) => {
         totalPrice: 0,
     })
 
+    const addProductToCart = (count: number, price: number) => {
+        setCartData((prevState: CartData) => ({
+            totalCount: prevState.totalCount + count,
+            totalPrice: prevState.totalPrice + price * count,
+        }))
+    }
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header cartData={cartData} />
-            <Main />
+            <Main addProductToCart={addProductToCart} />
         </StyledEngineProvider>
     )
 }
