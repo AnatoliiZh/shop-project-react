89635f91cd1d2e73598b86e4c69ba53e3ef224ec
AnatoliiZh <abcland2014@gmail.com>
2023-01-04 20:22:31 +0200

    add pages

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,5 +1,10 @@
 import { Container } from '@mui/material'
+import CartPage from 'Pages/CartPage/CartPage'
+import AboutPage from 'Pages/AboutPage/AboutPage'
+import ShippingPage from 'Pages/ShippingPage/ShippingPage'
+import PaymentPage from 'Pages/PaymentPage/PaymentPage'
 import Home from 'Pages/Home/Home'
+import { Routes, Route } from 'react-router-dom'
 
 type Props = {
     addProductToCart: (id: number, count: number) => void
@@ -14,7 +19,16 @@ const Main = ({ addProductToCart }: Props) => {
             }}
             component="main"
         >
-            <Home addProductToCart={addProductToCart} />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home addProductToCart={addProductToCart} />}
+                />
+                <Route path="about" element={<AboutPage />} />
+                <Route path="shipping" element={<ShippingPage />} />
+                <Route path="payment" element={<PaymentPage />} />
+                <Route path="cart" element={<CartPage />} />
+            </Routes>
         </Container>
     )
 }
