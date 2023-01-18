6bee29ac100f9bff2bfc8faa0447b792e4ab7bd7
AnatoliiZh <abcland2014@gmail.com>
2023-01-18 23:15:35 +0200

    begin

--- src/Pages/PaymentPage/PaymentPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-const PaymentPage = (props: Props) => {
-    return <div>PaymentPage</div>
-}
-
-export default PaymentPage
--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,9 +1,6 @@
-import CssBaseline from '@mui/material/CssBaseline'
-import Header from 'container/Header/Header'
-import { StyledEngineProvider } from '@mui/material/styles'
-import Main from 'container/Main/Main'
+
 import { useState } from 'react'
-import { omit } from 'lodash'
+
 
 type ProductsInCart = {
     [id: number]: number
@@ -15,31 +12,20 @@ const App = () => {
     const addProductToCart = (id: number, count: number) => {
         setProductsInCart((prevState: ProductsInCart) => ({
             ...prevState,
-            [id]: (prevState[id] || 0) + count,
+            [id]: prevState[id] + count,
         }))
     }
 
-    const removeProductFromCart = (id: number) => {
-        setProductsInCart(
-            (prevState: ProductsInCart) => omit(prevState, [id])
-            // let prevProductsInCart = { ...prevState }
-            // delete prevProductsInCart[id]
-            // return prevProductsInCart
-        )
-    }
+   
 
     return (
-        <StyledEngineProvider injectFirst>
-            <CssBaseline />
-            <Header productsInCart={productsInCart} />
-            {/* <button onClick={() => removeProductFromCart(1)}>Delete</button> */}
-            {/* <button onClick={() => addProductToCart(1, 1)}>Add to cart</button> */}
-            <Main
-                addProductToCart={addProductToCart}
-                productsInCart={productsInCart}
-                removeProductFromCart={removeProductFromCart}
-            />
-        </StyledEngineProvider>
+       <div>
+
+           
+            <button onClick={() => addProductToCart(1, 1)}>Add to cart {productsInCart[1]}</button> 
+            {/* <button onClick={() => addProductToCart(2, 1)}>Add to cart {productsInCart[2]}</button>  */}
+       </div>
+        
     )
 }
 export default App
