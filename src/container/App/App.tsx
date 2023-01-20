21c50d10b7f67a8953e82373089056f337ed925c
AnatoliiZh <abcland2014@gmail.com>
2023-01-20 13:25:43 +0200

    version 2

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,31 +1,26 @@
 import { useState } from 'react'
 import './App.scss'
 
-type Counter = {
-    [numberCounter: number]: number
-}
+// type Counter = {
+//     [numberCounter: number]: number
+// }
 
 const App = () => {
-    const [counter, setCounter] = useState<Counter>({})
-
-    const changeCounter = (numberCounter: number) => {
-        setCounter((prevState: Counter) => ({
-            ...prevState,
-            [numberCounter]: ++prevState[numberCounter] || 1,
-        }))
-    }
+    const [counter1, setCounter1] = useState(0)
+    const [counter2, setCounter2] = useState(0)
+    const [counter3, setCounter3] = useState(0)
 
     return (
         <div className="task-1">
             <h2>Перше завдання:</h2>
-            <button onClick={() => changeCounter(1)}>
-                Change count {counter[1] || 0}
+            <button onClick={() => setCounter1(counter1 + 1)}>
+                Change count {counter1}
             </button>
-            <button onClick={() => changeCounter(2)}>
-                Change count {counter[2] || 0}
+            <button onClick={() => setCounter2(counter2 + 1)}>
+                Change count {counter2}
             </button>
-            <button onClick={() => changeCounter(3)}>
-                Change count {counter[3] || 0}
+            <button onClick={() => setCounter3(counter3 + 1)}>
+                Change count {counter3}
             </button>
         </div>
     )
