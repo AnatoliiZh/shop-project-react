b8d8547442258091f050ef0fd602252c5efad0df
AnatoliiZh <abcland2014@gmail.com>
2023-01-29 09:48:55 +0200

    error NaN

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -2,15 +2,9 @@ import CartProductList from 'components/CartProductList/CartProductList'
 import CartTotal from 'components/CartTotal/CartTotal'
 import { useAppSelector } from 'redux/hooks'
 
-// type Props = {
-//     productsInCart: {
-//         [id: number]: number
-//     }
-// }
-
 const CartHeader = () => {
-    // console.log(productsArray[0].title)
     const productsInCart = useAppSelector((state) => state.productsInCart)
+    console.log(productsInCart)
     return (
         <div>
             <CartProductList productsInCart={productsInCart} />
